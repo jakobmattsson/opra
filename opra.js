ad53b7cfb71f0fbedb6870dcf3840dbbc1cc1142
Jakob Mattsson <jakob.mattsson@gmail.com>
2012-03-13 14:56:16 +0100

    Moved generic functions

--- opra.js
+++ opra.js
@@ -9,6 +9,89 @@ var uglify = require('uglify-js');
 var cleanCSS = require('clean-css');
 var glob = require('glob');
 
+var isUndefined = function(x) {
+  return typeof x == 'undefined';
+};
+var compileCoffee = function(filePath, encoding, callback) {
+  fs.readFile(filePath, encoding, function(err, content) {
+    var code = null;
+
+    if (err) {
+      callback(err);
+      return;
+    }
+
+    try {
+      code = coffee.compile(content);
+    } catch (e) {
+      callback(e);
+      return;
+    }
+
+    callback(null, code);
+  });
+};
+var compileLess = function(filePath, encoding, callback) {
+  fs.readFile(filePath, encoding, function(err, content) {
+    if (err) {
+      callback(err);
+      return;
+    }
+    less.render(content, { paths: [path.dirname(filePath)] }, callback);
+  });
+};
+var uglifier = function(code) {
+  var jsp = uglify.parser;
+  var pro = uglify.uglify;
+
+  var ast = jsp.parse(code);
+  ast = pro.ast_mangle(ast);
+  ast = pro.ast_squeeze(ast);
+  return pro.gen_code(ast);
+};
+var endsWith = function(str, ends) {
+  return ends.some(function(end) {
+    return end == str.slice(-end.length);
+  });
+};
+var safeReplace = function(str, target, newString) {
+  var i = str.indexOf(target);
+  return str.slice(0, i) + newString + str.slice(i + target.length);
+};
+var safeReplaceAll = function(str, target, newString) {
+  while (true) {
+    var i = str.indexOf(target);
+
+    if (i === -1) {
+      return str;
+    }
+
+    str = str.slice(0, i) + newString + str.slice(i + target.length);
+  }
+};
+var execAll = function(regexp, str) {
+  var match;
+  var matches = [];
+
+  while ((match = regexp.exec(str)) != null) {
+    matches.push(match);
+  }
+
+  return matches;
+};
+var removeIndex = function(array, index) {
+  var a = array.slice(0);
+  a.splice(index, 1);
+  return a;
+};
+var removeElement = function(array, element) {
+  var i = array.indexOf(element);
+  if (i !== -1) {
+    return removeIndex(array, i);
+  }
+  return array;
+};
+
 var build = function(indexFile, settings, callback) {
 
   if (!callback && typeof settings == 'function') {
@@ -28,90 +111,6 @@ var build = function(indexFile, settings, callback) {
     paths: !!settings.paths,
     ids: !!settings.ids
   };
-
-  var isUndefined = function(x) {
-    return typeof x == 'undefined';
-  };
-  var compileCoffee = function(filePath, callback) {
-    fs.readFile(filePath, encoding, function(err, content) {
-      var code = null;
-
-      if (err) {
-        callback(err);
-        return;
-      }
-
-      try {
-        code = coffee.compile(content);
-      } catch (e) {
-        callback(e);
-        return;
-      }
-
-      callback(null, code);
-    });
-  };
-  var compileLess = function(filePath, callback) {
-    fs.readFile(filePath, encoding, function(err, content) {
-      if (err) {
-        callback(err);
-        return;
-      }
-      less.render(content, { paths: [path.dirname(filePath)] }, callback);
-    });
-  };
-  var uglifier = function(code) {
-    var jsp = uglify.parser;
-    var pro = uglify.uglify;
-
-    var ast = jsp.parse(code);
-    ast = pro.ast_mangle(ast);
-    ast = pro.ast_squeeze(ast);
-    return pro.gen_code(ast);
-  };
-  var endsWith = function(str, ends) {
-    return ends.some(function(end) {
-      return end == str.slice(-end.length);
-    });
-  };
-  var safeReplace = function(str, target, newString) {
-    var i = str.indexOf(target);
-    return str.slice(0, i) + newString + str.slice(i + target.length);
-  };
-  var safeReplaceAll = function(str, target, newString) {
-    while (true) {
-      var i = str.indexOf(target);
-
-      if (i === -1) {
-        return str;
-      }
-
-      str = str.slice(0, i) + newString + str.slice(i + target.length);
-    }
-  };
-  var execAll = function(regexp, str) {
-    var match;
-    var matches = [];
-
-    while ((match = regexp.exec(str)) != null) {
-      matches.push(match);
-    }
-
-    return matches;
-  };
-  var removeIndex = function(array, index) {
-    var a = array.slice(0);
-    a.splice(index, 1);
-    return a;
-  };
-  var removeElement = function(array, element) {
-    var i = array.indexOf(element);
-    if (i !== -1) {
-      return removeIndex(array, i);
-    }
-    return array;
-  };
-
 
   var filetype = function(filename) {
     if (endsWith(filename, ['.css', '.less'])) {
@@ -261,9 +260,9 @@ var build = function(indexFile, settings, callback) {
       };
 
       if (endsWith(file.name, ['.less'])) {
-        compileLess(filePath, actualCallback);
+        compileLess(filePath, encoding, actualCallback);
       } else if (endsWith(file.name, ['.coffee'])) {
-        compileCoffee(filePath, actualCallback);
+        compileCoffee(filePath, encoding, actualCallback);
       } else {
         fs.readFile(filePath, encoding, actualCallback);
       }
@@ -357,7 +356,6 @@ var build = function(indexFile, settings, callback) {
 
       matches.forEach(function(m) {
         m.files.forEach(function(f) {
-
           ['compress', 'paths', 'ids'].forEach(function(n) {
             if (f.params.indexOf('always-' + n) !== -1 || (f.params.indexOf('never-' + n) === -1 && globalFlags[n])) {
               f.params.push(n);
