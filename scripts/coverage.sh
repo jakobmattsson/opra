d55a7ec3daca64a3258f8ff4d83350bc4100077e
Jakob Mattsson <jakob.mattsson@gmail.com>
2012-03-27 16:35:36 +0100

    Update test coverage code

---
+++ scripts/coverage.sh
@@ -0,0 +1,6 @@
+mkdir -p test-coverage
+rm -rf test-cov
+node-jscoverage src/ test-cov
+SRC_DIR=test-cov mocha --require spec/lib.js --reporter html-cov $1 > test-coverage/$2
+rm -rf test-cov
+open test-coverage/$2
